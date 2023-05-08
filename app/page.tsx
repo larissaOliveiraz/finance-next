1e159069ce1378827f53f366662ee055fb28d57f
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-05-08 16:47:18 -0300

    fix: add scroll on transitions table, change style

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ import { Trash } from "@phosphor-icons/react";
 
 export default function Home() {
    return (
-      <div>
-         <main className="absolute flex w-3/5 gap-5 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-            <section className="w-64">
+      <div className="flex h-screen m-auto overflow-x-hidden w">
+         <main className="flex w-3/5 gap-5 m-auto ">
+            <section className="fixed top-0 bottom-0 left-0 flex items-center bg-gray-100 rounded-xl w-96">
                <Dashboard />
             </section>
-            <section className="flex-1 bg-gray-100 rounded-xl">
+            <section className="flex-1 pt-3 mt-32 ml-64 rounded-xl">
                <Header />
                <table className="w-full">
-                  <tbody className="w-full">
+                  <tbody className="w-full overflow-y-auto">
                      {transactions.map((item) => (
                         <tr
                            className={`flex gap-8 p-3 m-3 border rounded-lg   ${
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -3,9 +3,9 @@ import { ArrowDown, ArrowUp, PiggyBank } from "@phosphor-icons/react";
 
 export const Dashboard = () => {
    return (
-      <section className="space-y-5">
+      <section className="fixed w-1/4 space-y-5 -translate-y-1/2 left-32 top-1/2">
          {/* INCOME CARD */}
-         <div className="w-full p-5 space-y-5 text-gray-100 bg-green-500 rounded-xl">
+         <div className="w-full space-y-5 text-gray-100 bg-green-500 p-7 rounded-xl">
             <div className="flex items-center gap-3">
                <ArrowUp size={24} weight="bold" />
                <h2 className="text-2xl font-semibold">Income</h2>
@@ -14,7 +14,7 @@ export const Dashboard = () => {
          </div>
 
          {/* OUTCOME CARD */}
-         <div className="min-w-full p-5 space-y-5 text-gray-100 bg-red-500 rounded-xl">
+         <div className="min-w-full space-y-5 text-gray-100 bg-red-500 p-7 rounded-xl">
             <div className="flex items-center gap-3">
                <ArrowDown size={24} weight="bold" />
                <h2 className="text-2xl font-semibold">Outcome</h2>
@@ -23,7 +23,7 @@ export const Dashboard = () => {
          </div>
 
          {/* TOTAL CARD */}
-         <div className="min-w-full p-5 space-y-5 text-gray-100 bg-indigo-500 rounded-xl">
+         <div className="min-w-full space-y-5 text-gray-100 bg-indigo-500 p-7 rounded-xl">
             <div className="flex items-center gap-3">
                <PiggyBank size={24} weight="bold" />
                <h2 className="text-2xl font-semibold">Total</h2>
