e285931b530e46d71bd48303084a5175e3ce0533
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-05-09 18:51:52 -0300

    fix: adds formatter to transactions table view

--- components/TransactionTable.tsx
+++ components/TransactionTable.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { TransactionContext } from "@/context/TransactionContext";
+import { useFormatter } from "@/hooks/useFormatter";
 import { Transaction } from "@/types/Transaction";
 import { Trash } from "@phosphor-icons/react";
 import React, { useContext } from "react";
 
 export const TransactionTable = () => {
    const { transactions } = useContext(TransactionContext);
+   const formatter = useFormatter();
 
    return (
       <div className="lg:h-[380px] md:h-[450px] overflow-auto scrollbar-hidden mt-4">
@@ -22,7 +24,9 @@ export const TransactionTable = () => {
                   >
                      <td className="flex-1">{item.title}</td>
                      <td className="min-w-[50px]">{item.category}</td>
-                     <td className="min-w-[50px]">{item.amount}</td>
+                     <td className="min-w-[50px]">
+                        {formatter.priceFormatter(item.amount)}
+                     </td>
                      <td className="flex items-center text-gray-600 hover:text-red-600">
                         <Trash size={22} weight="fill" />
                      </td>
